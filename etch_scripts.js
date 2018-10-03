a50430e213ce2ecafbdcaeeb63d731397764d51f
Jake Kile <jkile9@gmail.com>
2018-10-03 13:13:05 -0700

    update

---
+++ etch_scripts.js
@@ -0,0 +1,12 @@
+
+for(var i = 0; i < 16; i++){
+  var newDivHor = document.createElement('div');
+  newDivHor.id = 'box' + i;
+  newDivHor.class = 'grid'
+
+}
+for(var j = 0; j < 16; j++){
+  var newDivVert = document.createElement('div');
+  newDivVert.id = 'box' + j;
+  newDivVert.class = 'grid';
+}
